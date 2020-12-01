592592b936d151d69d236889dd357586e09fd140
hirano masaki <studio.rookery@gmail.com>
2020-12-02 01:46:06 +0900

    Add Combine extension

---
+++ Sources/Path/PathObserver+Combine.swift
@@ -0,0 +1,58 @@
+//
+//  File.swift
+//  
+//
+//  Created by masaki on 2020/12/02.
+//
+
+#if canImport(Combine)
+
+import Foundation
+import Combine
+
+@available(iOS 13.0, *)
+public extension PathObserver {
+    
+    struct Publisher: Combine.Publisher {
+        
+        public typealias Output = Path
+        public typealias Failure = Never
+        
+        private let pathObserver: PathObserver
+
+        public init(pathObserver: PathObserver) {
+            self.pathObserver = pathObserver
+        }
+        
+        public init(path: Path) {
+            self.pathObserver = PathObserver(for: path)
+        }
+        
+        public func receive<S>(subscriber: S) where S : Combine.Subscriber, Failure == S.Failure, Output == S.Input {
+            let subscription = Subscription(pathObserver: pathObserver)
+            subscriber.receive(subscription: subscription)
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+private extension PathObserver  {
+    
+    final class Subscription: Combine.Subscription {
+        
+        private let pathObserver: PathObserver
+
+        init(pathObserver: PathObserver) {
+            self.pathObserver = pathObserver
+        }
+
+        func request(_ demand: Subscribers.Demand) {
+            
+        }
+            
+        func cancel() {
+            pathObserver.stop()
+        }
+    }
+}
+#endif
